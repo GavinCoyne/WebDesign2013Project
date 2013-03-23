34e874e29e43d26138f8a12d77a3df8448df32b0
Gavin Coyne <speed.sk8@gmail.com>
2013-03-23 13:25:15 -0700

    commit

--- scripts/form.js
+++ scripts/form.js
@@ -4,7 +4,7 @@ function formCtrl($scope){
 
 
 }
-function CtrlB($scope){
+function Ctrl($scope){
     $scope.$parent;//is the CtrlA.ngInclude, not the CtrlA
     $scope.$parent.$parent;//is the CtrlA
 
