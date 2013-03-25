134f37dba1b933c4ca8ed1bdf8ef671019c0997b
Gavin Coyne <speed.sk8@gmail.com>
2013-03-25 12:53:30 -0700

    commit

--- scripts/ksi.js
+++ scripts/ksi.js
@@ -1,14 +1,14 @@
-function startTime()
+function startTime(id)
 {
-var today=new Date();
-var h=today.getHours();
-var m=today.getMinutes();
-var s=today.getSeconds();
-// add a zero in front of numbers<10
-m=checkTime(m);
-s=checkTime(s);
-document.getElementById('txt').innerHTML=h+":"+m+":"+s;
-t=setTimeout(function(){startTime()},500);
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    // add a zero in front of numbers<10
+    m=checkTime(m);
+    s=checkTime(s);
+    document.getElementById(id).innerHTML=h+":"+m+":"+s;
+    t=setTimeout(function(){startTime(id)},1000);
 }
 
 function checkTime(i)
@@ -38,8 +38,21 @@ function validateFormSubmit()
 		alert("Please Enter a valid email.");
 		return false;
 	}
-	
 }
+
+function clearImage(){
+    document.getElementById("fullImage").innerHTML = "";
+}
+
+
+function loadImage(srcImg, imgName){
+    document.getElementById("fullImage").innerHTML =
+        "<div class='imgContainer' onclick='clearImage()' >" +
+        "<h1>"+imgName+"</h1><br /><img class='fullImageImg' id="+imgName+" style='z-index:21;' src=" + srcImg + " /></div>";
+
+
+}
+
 
 
 
